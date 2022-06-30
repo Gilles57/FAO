1908e7b731225ed8ec74e76d6889ecc8bbea4f45
Gilles SALMON <g.salmon@free.fr>
2022-06-30 16:00:34 +0200

    Page bien-être

--- src/Controller/LivredorController.php
+++ src/Controller/LivredorController.php
@@ -12,7 +12,6 @@ class LivredorController extends AbstractController
     public function index(): Response
     {
         return $this->render('livredor/index.html.twig', [
-            'controller_name' => 'LivredorController',
         ]);
     }
 }
