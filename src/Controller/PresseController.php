332dd40b8b31bb9debfcf59d9aa1ee41e13437e5
Gilles SALMON <g.salmon@free.fr>
2022-07-10 16:48:48 +0200

    Page Presse et Presse bis

--- src/Controller/PresseController.php
+++ src/Controller/PresseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class PresseController extends AbstractController
 {
     #[Route('/presse', name: 'app_presse')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('presse/presse.html.twig', [
-            'controller_name' => 'PresseController',
-        ]);
+        $articles = $articleRepository->findAll();
+
+        return $this->render('presse/presse.html.twig', compact('articles'));
+    }
+
+    #[Route('/presse2', name: 'app_presse2')]
+    public function indexbis(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('presse/presse2.html.twig', compact('articles'));
+    }
+
+
+    #[Route('/presse/show/{id}', name: 'app_presse_show')]
+    public function show($id, ArticleRepository $articleRepository): Response
+    {
+        $article = $articleRepository->findOneById($id);
+
+        return $this->render('presse/presse_show.html.twig', compact('article'));
     }
 }
