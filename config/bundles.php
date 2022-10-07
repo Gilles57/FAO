a31720a26263adcaf99f029b74f11fe1d4b3d59d
Gilles SALMON <g.salmon@free.fr>
2022-10-07 13:25:39 +0200

    Installation de knplabs/knp-paginator-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     CalendarBundle\CalendarBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
