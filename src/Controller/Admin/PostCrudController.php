ed6c1502b238d33879a498fba5b64d2abcfb72eb
Gilles SALMON <g.salmon@free.fr>
2022-11-16 12:58:27 +0100

    Quelques TODO

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -14,7 +14,7 @@ use Vich\UploaderBundle\Form\Type\VichImageType;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
-
+// TODO ckeditor
 class PostCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
+// TODO Captcha
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
