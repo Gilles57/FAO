800bf1e43a77afb3fab757f9f03942eebd54b445
Gilles57 <g.salmon@free.fr>
2023-02-04 17:07:32 +0100

    Correction des tooltips

--- tests/Functional/BlogPageTest.php
+++ tests/Functional/BlogPageTest.php
@@ -18,7 +18,7 @@ class BlogPageTest extends WebTestCase
 
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
 
-        $this->assertCount(0,  $crawler->filter('img.img-thumbnail'));
+        $this->assertCount(1,  $crawler->filter('img.img-thumbnail'));
         $this->assertCount(0,  $crawler->filter('.pagination'));
 
     }
